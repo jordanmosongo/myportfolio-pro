267882c2da1997589b0442962dc0f23517fb7279
jordanmosongo <mosongojordan@gmail.com>
2021-08-09 10:06:37 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { TitleComponent } from './components/shared/title/title.component';
+import { ButtonComponent } from './components/shared/button/button.component';
+import { CardComponent } from './components/shared/card/card.component';
+import { AComponent } from './exercices/a/a.component';
+import { BComponent } from './exercices/b/b.component';
+import { CComponent } from './exercices/c/c.component';
+import { ItemComponent } from './components/shared/item/item.component';
+import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    TitleComponent,
+    ButtonComponent,
+    CardComponent,
+    AComponent,
+    BComponent,
+    CComponent,
+    ItemComponent,
+    AboutComponent,
+      
   ],
   imports: [
     BrowserModule,
